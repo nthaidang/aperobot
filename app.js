47428c94a0aecb63bdc06d8f4356f03bfb2bcd48
nthaidang <dang.nguyentienhai@gmail.com>
2017-07-24 17:12:51 +0200

    :rocket: update env

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 const connector = new builder.ChatConnector({
-    appId: config.id,
-    appPassword: config.password
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users 
